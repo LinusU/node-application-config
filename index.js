13c517a63747ebd2fd5e9c5629d080f9cc0333cd
Feross Aboukhadijeh <feross@feross.org>
2016-04-16 00:17:25 -0700

    Let filePath be changed at runtime

--- index.js
+++ index.js
@@ -3,58 +3,54 @@ var path = require('path')
 var mkdirp = require('mkdirp')
 var applicationConfigPath = require('application-config-path')
 
-function applicationConfig (name) {
-  var directoryPath = applicationConfigPath(name)
-  var filePath = path.join(directoryPath, 'config.json')
-
-  function read (cb) {
-    fs.readFile(filePath, function (err, raw) {
-      if (err && err.code === 'ENOENT') return cb(null, {})
-      if (err) return cb(err)
-
-      var data
-      try {
-        data = JSON.parse(raw.toString())
-      } catch (err) {
-        return cb(err)
-      }
-
-      cb(null, data)
-    })
-  }
-
-  function trash (cb) {
-    fs.unlink(filePath, function (err) {
-      if (err && err.code !== 'ENOENT') return cb(err)
-
-      fs.rmdir(directoryPath, function (err) {
-        if (err && err.code !== 'ENOENT') return cb(err)
-
-        cb(null)
-      })
-    })
-  }
-
-  function write (data, cb) {
-    mkdirp(directoryPath, function (err) {
-      if (err) { return cb(err) }
-
-      fs.writeFile(filePath, JSON.stringify(data), cb)
-    })
-  }
-
-  return {
-    filePath: filePath,
-    read: read,
-    trash: trash,
-    write: function (data, cb) {
-      if (typeof data !== 'object' || data === null) {
-        throw new TypeError('data is not an object')
-      }
-
-      return write(data, cb)
-    }
-  }
+function ApplicationConfig (name) {
+  this.filePath = path.join(applicationConfigPath(name), 'config.json')
 }
 
-module.exports = applicationConfig
+ApplicationConfig.prototype.read = function (cb) {
+  var self = this
+  fs.readFile(self.filePath, function (err, raw) {
+    if (err && err.code === 'ENOENT') return cb(null, {})
+    if (err) return cb(err)
+
+    var data
+    try {
+      data = JSON.parse(raw.toString())
+    } catch (err) {
+      return cb(err)
+    }
+
+    cb(null, data)
+  })
+}
+
+ApplicationConfig.prototype.write = function (data, cb) {
+  var self = this
+  if (typeof data !== 'object' || data === null) {
+    throw new TypeError('data is not an object')
+  }
+  var directoryPath = path.dirname(self.filePath)
+  mkdirp(directoryPath, function (err) {
+    if (err) { return cb(err) }
+
+    fs.writeFile(self.filePath, JSON.stringify(data), cb)
+  })
+}
+
+ApplicationConfig.prototype.trash = function (cb) {
+  var self = this
+  fs.unlink(self.filePath, function (err) {
+    if (err && err.code !== 'ENOENT') return cb(err)
+
+    var directoryPath = path.dirname(self.filePath)
+    fs.rmdir(directoryPath, function (err) {
+      if (err && err.code !== 'ENOENT') return cb(err)
+
+      cb(null)
+    })
+  })
+}
+
+module.exports = function createApplicationConfig (name) {
+  return new ApplicationConfig(name)
+}
