ad1d12cf599f50084bd17f681bbec68561d47a54
Linus Unnebäck <linus@folkdatorn.se>
2019-11-16 17:22:16 +0000

    💥 Change API to return Promises
    Migration Guide:
    
    Every function that previously accepted a callback now returns a `Promise` instead. See the updated documentation for details.

--- index.js
+++ index.js
@@ -1,64 +1,45 @@
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
+
 const applicationConfigPath = require('application-config-path')
+const loadJsonFile = require('load-json-file')
+
+const rmdir = promisify(fs.rmdir)
+const unlink = promisify(fs.unlink)
 
 class ApplicationConfig {
   constructor (name) {
     this.filePath = path.join(applicationConfigPath(name), 'config.json')
   }
 
-  read (cb) {
-    fs.readFile(this.filePath, (err, raw) => {
-      if (err && err.code === 'ENOENT') return cb(null, {})
-      if (err) return cb(err)
-
-      let data
-      try {
-        data = JSON.parse(raw.toString())
-      } catch (err) {
-        return cb(err)
-      }
-
-      cb(null, data)
-    })
+  async read () {
+    try {
+      return await loadJsonFile(this.filePath)
+    } catch (err) {
+      if (err.code === 'ENOENT') return {}
+      throw err
+    }
   }
 
-  write (data, cb) {
+  async write (data) {
     if (typeof data !== 'object' || data === null) {
       throw new TypeError('data is not an object')
     }
 
-    const mkdirp = require('mkdirp')
-    const directoryPath = path.dirname(this.filePath)
+    const writeJsonFile = require('write-json-file')
 
-    mkdirp(directoryPath, (err) => {
-      if (err) return cb(err)
-
-      const tempFilePath =
-        this.filePath + '-' +
-        Math.random().toString().substr(2) +
-        Date.now().toString() +
-        path.extname(this.filePath)
-
-      fs.writeFile(tempFilePath, JSON.stringify(data, null, 2), (err) => {
-        if (err) return cb(err)
-
-        fs.rename(tempFilePath, this.filePath, cb)
-      })
-    })
+    await writeJsonFile(this.filePath, data)
   }
 
-  trash (cb) {
-    fs.unlink(this.filePath, (err) => {
-      if (err && err.code !== 'ENOENT') return cb(err)
-
-      const directoryPath = path.dirname(this.filePath)
-      fs.rmdir(directoryPath, (err) => {
-        if (err && err.code !== 'ENOENT') return cb(err)
-
-        cb(null)
-      })
-    })
+  async trash () {
+    try {
+      await unlink(this.filePath)
+      await rmdir(path.dirname(this.filePath))
+    } catch (err) {
+      if (err.code === 'ENOENT') return
+      throw err
+    }
   }
 }
 
