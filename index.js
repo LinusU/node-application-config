c7f9d4f45523c0541eb748a84e8982b160fc7bc9
Linus Unnebäck <linus@folkdatorn.se>
2019-11-16 14:22:45 +0000

    💥 Drop support for Node.js <8.3
    Migration Guide:
    
    Upgrade to Node.js 8.3.0 or newer for continued support.

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
-var fs = require('fs')
-var path = require('path')
-var applicationConfigPath = require('application-config-path')
+const fs = require('fs')
+const path = require('path')
+const applicationConfigPath = require('application-config-path')
 
-function ApplicationConfig (name) {
-  this.filePath = path.join(applicationConfigPath(name), 'config.json')
-}
+class ApplicationConfig {
+  constructor (name) {
+    this.filePath = path.join(applicationConfigPath(name), 'config.json')
+  }
 
-ApplicationConfig.prototype.read = function (cb) {
-  var self = this
-  fs.readFile(self.filePath, function (err, raw) {
-    if (err && err.code === 'ENOENT') return cb(null, {})
-    if (err) return cb(err)
+  read (cb) {
+    fs.readFile(this.filePath, (err, raw) => {
+      if (err && err.code === 'ENOENT') return cb(null, {})
+      if (err) return cb(err)
 
-    var data
-    try {
-      data = JSON.parse(raw.toString())
-    } catch (err) {
-      return cb(err)
+      let data
+      try {
+        data = JSON.parse(raw.toString())
+      } catch (err) {
+        return cb(err)
+      }
+
+      cb(null, data)
+    })
+  }
+
+  write (data, cb) {
+    if (typeof data !== 'object' || data === null) {
+      throw new TypeError('data is not an object')
     }
 
-    cb(null, data)
-  })
-}
+    const mkdirp = require('mkdirp')
+    const directoryPath = path.dirname(this.filePath)
 
-ApplicationConfig.prototype.write = function (data, cb) {
-  var self = this
-  var mkdirp = require('mkdirp')
-  if (typeof data !== 'object' || data === null) {
-    throw new TypeError('data is not an object')
-  }
-  var directoryPath = path.dirname(self.filePath)
-  mkdirp(directoryPath, function (err) {
-    if (err) { return cb(err) }
+    mkdirp(directoryPath, (err) => {
+      if (err) return cb(err)
 
-    var tempFilePath =
-      self.filePath + '-' +
-      Math.random().toString().substr(2) +
-      Date.now().toString() +
-      path.extname(self.filePath)
+      const tempFilePath =
+        this.filePath + '-' +
+        Math.random().toString().substr(2) +
+        Date.now().toString() +
+        path.extname(this.filePath)
 
-    fs.writeFile(tempFilePath, JSON.stringify(data, null, 2), function (err) {
-      if (err) { return cb(err) }
+      fs.writeFile(tempFilePath, JSON.stringify(data, null, 2), (err) => {
+        if (err) return cb(err)
 
-      fs.rename(tempFilePath, self.filePath, cb)
+        fs.rename(tempFilePath, this.filePath, cb)
+      })
     })
-  })
-}
+  }
 
-ApplicationConfig.prototype.trash = function (cb) {
-  var self = this
-  fs.unlink(self.filePath, function (err) {
-    if (err && err.code !== 'ENOENT') return cb(err)
-
-    var directoryPath = path.dirname(self.filePath)
-    fs.rmdir(directoryPath, function (err) {
+  trash (cb) {
+    fs.unlink(this.filePath, (err) => {
       if (err && err.code !== 'ENOENT') return cb(err)
 
-      cb(null)
+      const directoryPath = path.dirname(this.filePath)
+      fs.rmdir(directoryPath, (err) => {
+        if (err && err.code !== 'ENOENT') return cb(err)
+
+        cb(null)
+      })
     })
-  })
+  }
 }
 
 module.exports = function createApplicationConfig (name) {
